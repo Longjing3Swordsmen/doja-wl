32a060d45e692f3d6370835acf67458f8b1695cf
vonhng <qianyong.feng@woqutech.com>
2019-09-06 09:34:11 +0800

    update install.sh

--- install.sh
+++ install.sh
@@ -7,11 +7,14 @@ doja=$dir/doja
 mkdir $doja
 mkdir -p /var/run/supervisor
 
+mkdir -p logs apps conf /var/run/supervisor/
 cp -r apps conf logs supervisord $doja
 chown -R root:root .*
 
-supervisord -c $doja/supervisord/supervisord.conf
-echo "alias mysuper='supervisorctl -c /root/doja/supervisord/supervisord.conf'" >> ~/.bashrc
-source ~/.bashrc
+supervisord -c $doja/supervisord/supervisord.conf > /dev/null 2>&1
+echo "alias mysuper='supervisorctl -c $doja/supervisord/supervisord.conf'" >> ~/.bashrc
+echo "alias mystart='supervisord -c $doja/supervisord/supervisord.conf'" >> ~/.bashrc
+source /root/.bashrc
+mysuper status
 
 
